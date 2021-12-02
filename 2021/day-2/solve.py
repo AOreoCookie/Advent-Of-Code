0991de9101a9ded8a2b7055c25d38ef71c058f78
AOreoCookie <masonnunn.00@gmail.com>
2021-12-02 12:43:38 -0500

    Day 2 Complete

--- 2021/day-2/solve.py
+++ 2021/day-2/solve.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 
+from re import search, match
+
 def main():
-  upDowns = [(line.split()[0], -int(line.rstrip().split()[1])) if line.split()[0] == "up" else (line.split()[0], int(line.rstrip().split()[1])) for line in open("input.txt")]
-  p1 = sum(int(line.rstrip().split()[1]) for line in open("input.txt") if line.split()[0] == "forward") * sum(tup[1] for tup in upDowns)
+  nP = r'[0-9]+'
+  upDowns = sum(-int(search(nP, line).group()) for line in open("input.txt") if bool(match("up", line))) + sum(int(search(nP, line).group()) for line in open("input.txt") if bool(match("down", line)))
+  p1 = sum(int(search(nP, line).group()) for line in open("input.txt") if bool(match("forward", line))) * upDowns
 
   (x, y, aim) = (0, 0, 0)
   for line in open("input.txt"):
@@ -13,19 +16,5 @@ def main():
     
   print(f"--------Advent of Code 2021 [Day 2]--------")
   print(f"[X] Part 1 Answer: {p1}\n[X] Part 2 Answer: {p2}")
-
+  
 if __name__ == "__main__": main()
-
-
-"""
---- Part One ---
-with open('input.txt') as f:
-  for line in f:
-    line = line.rstrip().split(" ")
-    (dir, amount) = (line[0], int(line[1]))
-    if dir == "forward": x = x + amount
-    elif dir == "up": y = y - amount
-    else: y = y + amount
-
-
-"""
